5072e86d334aecd0645a1c219ab412c2615c4144
mc-cat-tty <44820563+mc-cat-tty@users.noreply.github.com>
2024-07-07 17:11:11 +0200

    Add test function to check if scope is implemented correctly

--- test/callglobal.cpp
+++ test/callglobal.cpp
@@ -1,12 +1,10 @@
 #include <iostream>
 
-extern "C" {
-  double f();
-  double GLOBAL;
-}
+extern "C" double f(), g(), GLOBAL;
 
 int main() {
   GLOBAL = 20;
   std::cout << "f(): " << f() << std::endl;
+  std::cout << "g(): " << g() << std::endl;
   std::cout << "GLOBAL: " << GLOBAL << std::endl;
 }
