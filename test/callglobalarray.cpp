d6603cd041622d740dfe260197a0b5cc9fd163af
mc-cat-tty <44820563+mc-cat-tty@users.noreply.github.com>
2024-07-09 02:51:26 +0200

    Test edgecase

--- test/callglobalarray.cpp
+++ test/callglobalarray.cpp
@@ -5,7 +5,7 @@ constexpr size_t size = 10;
 extern "C" double f(double i), val[size] = {1, 2, 3, 4};
 
 int main() {
-  for (int i = 0; i < size; i++)
+  for (int i = 0; i < size-1; i++)
     std::cout << f(i) << std::endl;
   
   std::cout << std::endl;
