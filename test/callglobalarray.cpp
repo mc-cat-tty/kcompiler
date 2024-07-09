3d21bc6756561ec97d58738f5b824b7fa48645ec
mc-cat-tty <44820563+mc-cat-tty@users.noreply.github.com>
2024-07-09 02:50:47 +0200

    Add test for global array

---
+++ test/callglobalarray.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+constexpr size_t size = 10;
+
+extern "C" double f(double i), val[size] = {1, 2, 3, 4};
+
+int main() {
+  for (int i = 0; i < size; i++)
+    std::cout << f(i) << std::endl;
+  
+  std::cout << std::endl;
+
+  for (int i = 0; i < size; i++)
+    std::cout << f(i) << std::endl;
+  return 0;
+}
